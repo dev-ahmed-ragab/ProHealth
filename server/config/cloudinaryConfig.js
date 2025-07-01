515be8a057830e842ac527d2efe40cf8d6acbc57
ahmed-ragab-dev <ahmedragab.dev@outlook.com>
2025-07-01 20:57:58 +0300

    add env

--- server/config/cloudinaryConfig.js
+++ server/config/cloudinaryConfig.js
@@ -4,10 +4,10 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: 'dn7s97ydy',
-  api_key: '661839662643331',
-  api_secret: 'GydXCIzBQBFptgSEfjvWrODNUnQ',
-});
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME || 'dn7s97ydy',
+    api_key: process.env.CLOUDINARY_API_KEY || '661839662643331',
+    api_secret: process.env.CLOUDINARY_API_SECRET || 'GydXCIzBQBFptgSEfjvWrODNUnQ',
+  });
 console.log('Cloudinary Config:', cloudinary.config());
 
 export default cloudinary;
